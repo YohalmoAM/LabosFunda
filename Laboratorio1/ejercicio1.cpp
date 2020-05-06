b2632ae08aab18afa45e73da2954f2840674b200
unknown <00011420@uca.edu.sv>
2020-05-06 11:52:57 -0600

    primer programa

---
+++ Laboratorio1/ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string concatenar;
+    string uno = "hola";
+    string dos = "mundo";
+
+    concatenar = uno + dos;
+
+    cout <<concatenar;
+
+    return 0;
+}
---
+++ Laboratorio1/ejercicio2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int numero;
+
+cout <<"ingresa un numero porfavor";
+cin >> numero;
+
+cout << numero;
+   
+    return 0;
+
+}
